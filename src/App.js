d9f0789b705c5ad0d639fde7fccc8d4e5f81a64b
Arush-Srivastava <arushadorable6@gmail.com>
2025-03-29 13:02:00 +0530

    Added Metric System

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import './index.css'
-import React, {useState} from 'react'
+import React, {useState,useRef} from 'react'
 
 function App() {
   // state
@@ -8,25 +8,42 @@ function App() {
   const [height, setHeight] = useState(0)
   const [bmi, setBmi] = useState('')
   const [message, setMessage] = useState('')
+  const chosenSystem = useRef();
 
   let calcBmi = (event) => {
     event.preventDefault()
     console.log(event);
-
-    if (weight === 0 || height === 0) {
-      alert('Please enter a valid weight and height')
-    } else {
-      let bmi = (weight / (height * height) * 703)
-      setBmi(bmi.toFixed(1))
-
-      if (bmi < 25) {
-        setMessage('You are underweight')
-      } else if (bmi >= 25 && bmi < 30) {
-        setMessage('You have healthy weight')
+    if (chosenSystem.current.value === 'Non Metric ( lbs,inches )') {
+      if (weight === 0 || height === 0) {
+        alert('Please enter a valid weight and height')
       } else {
-        setMessage('You are overweight')
+        let bmi = (weight/ (height*height) * 703)
+        setBmi(bmi.toFixed(1));
+
+        if (bmi < 25) {
+          setMessage('You are underweight')
+        } else if (bmi >= 25 && bmi < 30) {
+          setMessage('You have healthy weight')
+        } else {
+          setMessage('You are overweight')
+        }
+      }
+    } else {
+      if (weight === 0 || height === 0) {
+        alert('Please enter a valid weight and height')
+      } else {
+        let bmi = (weight/ (height*height))
+        setBmi(bmi.toFixed(1));
+        if (bmi < 25) {
+          setMessage('You are underweight')
+        } else if (bmi >= 25 && bmi < 30) {
+          setMessage('You have healthy weight')
+        } else{
+          setMessage('You are overweight')
+        }
       }
     }
+
   }
 
   let reload = () => {
@@ -40,12 +57,21 @@ function App() {
       <h2 className='center'>BMI Calculator</h2>
         <form onSubmit={calcBmi}>
           <div>
-            <label>Weight (lbs)</label>
-            <input type="text" placeholder='Enter Weight in lbs' value={weight} onChange={(e) => setWeight(e.target.value)} />
+            <label htmlFor="system">Select the System: 
+              <select id="system" ref={chosenSystem}>
+                <option>Metric ( kg,metres ) </option>
+                <option>Non Metric ( lbs,inches )</option>
+              </select>
+            </label>
+          </div>
+          <br></br><br></br>
+          <div>
+            <label>Weight</label>
+            <input type="text" placeholder='Enter Weight' value={weight} onChange={(e) => setWeight(e.target.value)} />
           </div>
           <div>
-            <label>Height (in)</label>
-            <input type="text" placeholder='Enter height in inches' value={height} onChange={(event) => setHeight(event.target.value)} />
+            <label>Height</label>
+            <input type="text" placeholder='Enter height' value={height} onChange={(event) => setHeight(event.target.value)} />
           </div>
           <div>
             <button className='btn' type='submit'>Submit</button>
