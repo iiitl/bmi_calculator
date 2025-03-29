800794a0dd3c8d8a28b2cbd337c08bf376f2c8da
Arush-Srivastava <arushadorable6@gmail.com>
2025-03-29 10:35:52 +0530

    Corrected the BMI standards

--- src/App.js
+++ src/App.js
@@ -19,12 +19,14 @@ function App() {
       let bmi = (weight / (height * height) * 703)
       setBmi(bmi.toFixed(1))
 
-      if (bmi < 25) {
+      if (bmi < 18.5) {
         setMessage('You are underweight')
-      } else if (bmi >= 25 && bmi < 30) {
+      } else if (bmi >= 18.5 && bmi < 25) {
         setMessage('You have healthy weight')
-      } else {
+      } else if(bmi>= 25 && bmi <30) {
         setMessage('You are overweight')
+      } else{
+        setMessage('You are obese')
       }
     }
   }
