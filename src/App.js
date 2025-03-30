0f8ecba9d487172493405411aa43010d67ea29c3
Prabhnoor Kaur <prabhnooorkaur11@gmail.com>
2025-03-30 22:34:45 +0530

    taking input in metric system

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ function App() {
   const [height, setHeight] = useState(0)
   const [bmi, setBmi] = useState('')
   const [message, setMessage] = useState('')
+  const [unit, setUnit] = useState('imperial'); 
+
 
   let calcBmi = (event) => {
     event.preventDefault()
@@ -16,7 +18,11 @@ function App() {
     if (weight === 0 || height === 0) {
       alert('Please enter a valid weight and height')
     } else {
-      let bmi = (weight / (height * height) * 703)
+      let bmi;
+      if( unit === 'imperial')
+      bmi = (weight / (height * height) * 703)
+      else
+      bmi = weight / (height * height);
       setBmi(bmi.toFixed(1))
 
       if (bmi < 18.5) {
@@ -42,15 +48,34 @@ function App() {
     <div className='container'>
       <h2 className='center'>BMI Calculator</h2>
         <form onSubmit={calcBmi}>
-          <div>
-            <label>Weight (lbs)</label>
-            <input type="text" placeholder='Enter Weight in lbs' value={weight} onChange={(e) => setWeight(e.target.value)} />
-          </div>
-          <div>
-            <label>Height (in)</label>
-            <input type="text" placeholder='Enter height in inches' value={height} onChange={(event) => setHeight(event.target.value)} />
-          </div>
-          <div>
+        <div className="unit">
+  <label>unit</label>
+  <select value={unit} onChange={(unitt) => setUnit(unitt.target.value)}>
+    <option value="imperial">Imperial (lbs, inches)</option>
+    <option value="metric">Metric (kg, meters)</option>
+  </select>
+</div>
+
+        <div>
+  <label>Weight ({unit === 'imperial' ? 'lbs' : 'kg'})</label>
+  <input 
+    type="text" 
+    placeholder={`Enter weight in ${unit === 'imperial' ? 'lbs' : 'kg'}`} 
+    value={weight} 
+    onChange={(wt) => setWeight(wt.target.value)} 
+  />
+</div>
+<div>
+  <label>Height ({unit === 'imperial' ? 'in' : 'm'})</label>
+  <input 
+    type="text" 
+    placeholder={`Enter height in ${unit === 'imperial' ? 'inches' : 'meters'}`} 
+    value={height} 
+    onChange={(ht) => setHeight(ht.target.value)} 
+  />
+</div>
+
+         <div>
             <button className='btn' type='submit'>Submit</button>
             <button className='btn btn-outline' onClick={reload} type='submit'>Reload</button>
           </div>
