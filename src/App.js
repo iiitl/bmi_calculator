521e8a616209d309daa279bb60a88fc43b47948b
vingoel26 <vinayakgoel012@gmail.com>
2025-03-30 14:53:36 +0530

    changes in input theme

--- src/App.js
+++ src/App.js
@@ -1,75 +1,37 @@
 import './App.css';
-import './index.css'
-import React, {useState} from 'react'
+import './index.css';
+import React, { useState } from 'react';
 
 function App() {
-  // state
-  const [weight, setWeight] = useState(0)
-  const [height, setHeight] = useState(0)
-  const [bmi, setBmi] = useState('')
-  const [message, setMessage] = useState('')
+  
   const [theme, setTheme] = useState('light');
 
   const toggleTheme = () => {
     setTheme(theme === 'light' ? 'dark' : 'light');
   };
 
-  let calcBmi = (event) => {
-    event.preventDefault()
-    console.log(event);
-
-    if (weight === 0 || height === 0) {
-      alert('Please enter a valid weight and height')
-    } else {
-      let bmi = (weight / (height * height) * 703)
-      setBmi(bmi.toFixed(1))
-
-      
-        if (bmi < 18.5) {
-          setMessage('You are underweight')
-        }  else if (bmi >= 18.5 && bmi < 25) {
-          setMessage('You have healthy weight')
-        } else if (bmi>=25 && bmi <30) {
-          setMessage('You are overweight')
-        }else{
-          setMessage('You are obese')
-        }
-      
-    }
-  }
-
-  let reload = () => {
-    window.location.reload()
-  }
-
-  
   return (
     <div className={`app ${theme}-theme`}>
-    <div className='container'>
-    <button className='theme-toggle-btn' onClick={toggleTheme}>
+      <div className='container'>
+        <button className='theme-toggle-btn' onClick={toggleTheme}>
           Switch to {theme === 'light' ? 'Dark' : 'Light'} Mode
         </button>
-      <h2 className='center'>BMI Calculator</h2>
-        <form onSubmit={calcBmi}>
+        <h2 className='center'>BMI Calculator</h2>
+        <form>
           <div>
             <label>Weight (lbs)</label>
-            <input type="text" placeholder='Enter Weight in lbs' value={weight} onChange={(e) => setWeight(e.target.value)} />
+            <input className={`input-field ${theme}-theme`} type='text' placeholder='Enter Weight in lbs' />
           </div>
           <div>
             <label>Height (in)</label>
-            <input type="text" placeholder='Enter height in inches' value={height} onChange={(event) => setHeight(event.target.value)} />
+            <input className={`input-field ${theme}-theme`} type='text' placeholder='Enter height in inches' />
           </div>
           <div>
             <button className='btn' type='submit'>Submit</button>
-            <button className='btn btn-outline' onClick={reload} type='submit'>Reload</button>
           </div>
         </form>
-        <div className='center'>
-          <h3>Your BMI is: {bmi}</h3>
-          <p>{message}</p>
-        </div>
+      </div>
     </div>
-  </div>
   );
 }
 
