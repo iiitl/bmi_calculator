fa8d71b3477a714e7e92aceaa66335fa6c5874c0
manav agarwal <manavagarwal578@gmail.com>
2025-03-30 08:57:14 +0530

    Added metric System Also

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import './index.css'
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 
 function App() {
   // state
@@ -8,6 +8,7 @@ function App() {
   const [height, setHeight] = useState(0)
   const [bmi, setBmi] = useState('')
   const [message, setMessage] = useState('')
+  const [unit, setUnit] = useState('lbs/in')
 
   let calcBmi = (event) => {
     event.preventDefault()
@@ -15,21 +16,25 @@ function App() {
 
     if (weight === 0 || height === 0) {
       alert('Please enter a valid weight and height')
-    } else {
-      let bmi = (weight / (height * height) * 703)
+    }
+    else if (unit === 'lbs/in') {
+      bmi = (weight / (height * height) * 703)
+    }
+    else {
+      let bmi = (weight / (height * height))
       setBmi(bmi.toFixed(1))
 
-      
-        if (bmi < 18.5) {
-          setMessage('You are underweight')
-        }  else if (bmi >= 18.5 && bmi < 25) {
-          setMessage('You have healthy weight')
-        } else if (bmi>=25 && bmi <30) {
-          setMessage('You are overweight')
-        }else{
-          setMessage('You are obese')
-        }
-      
+
+      if (bmi < 18.5) {
+        setMessage('You are underweight')
+      } else if (bmi >= 18.5 && bmi < 25) {
+        setMessage('You have healthy weight')
+      } else if (bmi >= 25 && bmi < 30) {
+        setMessage('You are overweight')
+      } else {
+        setMessage('You are obese')
+      }
+
     }
   }
 
@@ -37,19 +42,26 @@ function App() {
     window.location.reload()
   }
 
-  
+
   return (
     <div className="app">
-    <div className='container'>
-      <h2 className='center'>BMI Calculator</h2>
+      <div className='container'>
+        <h2 className='center'>BMI Calculator</h2>
         <form onSubmit={calcBmi}>
           <div>
-            <label>Weight (lbs)</label>
-            <input type="text" placeholder='Enter Weight in lbs' value={weight} onChange={(e) => setWeight(e.target.value)} />
+            <label>Select Unit</label>
+            <select value={unit} onChange={(e) => setUnit(e.target.value)}>
+              <option value="lbs/in">lbs/in</option>
+              <option value="kg/m">kg/m</option>
+            </select>
           </div>
           <div>
-            <label>Height (in)</label>
-            <input type="text" placeholder='Enter height in inches' value={height} onChange={(event) => setHeight(event.target.value)} />
+            <label>Weight {unit === "lbs/in" ? "(lbs)" : "(kg)"}</label>
+            <input type="text" placeholder={`Enter Weight in ${unit === 'lbs/in' ? 'pound' : 'kilogram'}`} value={weight} onChange={(e) => setWeight(e.target.value)} />
+          </div>
+          <div>
+            <label>Height {unit === "kg/m" ? "(m)" : "(in)"}</label>
+            <input type="text" placeholder={`Enter height in ${unit === "kg/m" ? "metre" : "inches"}`} value={height} onChange={(event) => setHeight(event.target.value)} />
           </div>
           <div>
             <button className='btn' type='submit'>Submit</button>
@@ -60,8 +72,8 @@ function App() {
           <h3>Your BMI is: {bmi}</h3>
           <p>{message}</p>
         </div>
+      </div>
     </div>
-  </div>
   );
 }
 
